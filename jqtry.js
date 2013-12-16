7d55c9d5d6884838817f57f14fe460188cb5d27a
Sarah Frier <zorginbagel@gmail.com>
2013-12-16 11:18:03 -0600

    upload code!

---
+++ jqtry.js
@@ -0,0 +1,11 @@
+$(document).ready();
+
+$("#box1").on("click", function () {
+    $(this).html('<div id="box1a"></div>');
+});
+$("#box2").on("click", function () {
+    $(this).html('<div id="box2a"></div>');
+});
+$("#box3").on("click", function () {
+    $(this).html('<div id="box3a"></div>');
+});
